e9a6dc2817b0bdfc7053bb623019f4cac98316ff
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2024-09-28 15:06:48 +1000

    remove bravo and demo-2

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -167,18 +167,10 @@ flux bootstrap github \
   --private=false \
   --timeout=10m0s
 
-# # Create public IP for XLB
-# gcloud compute addresses create static-mci-ip --global --project $PROJECT_ID
-# export STATIC_MCI_IP=`gcloud compute addresses describe static-mci-ip --project $PROJECT_ID --global --format="value(address)"`
-# echo -e "GCLB_IP is $STATIC_MCI_IP"
-
-# gcloud compute addresses create team-alpha-tenant-api --global --project $PROJECT_ID
-# export ALPHA_IP=`gcloud compute addresses describe team-alpha-tenant-api --project $PROJECT_ID --global --format="value(address)"`
-# echo -e "GCLB_IP is $ALPHA_IP"
-
-# gcloud compute addresses create team-bravo-tenant-api --global --project $PROJECT_ID
-# export BRAVO_IP=`gcloud compute addresses describe team-bravo-tenant-api --project $PROJECT_ID --global --format="value(address)"`
-# echo -e "GCLB_IP is $BRAVO_IP"
+# Create public IP for XLB
+gcloud compute addresses create team-alpha-tenant-api --global --project $PROJECT_ID
+export ALPHA_IP=`gcloud compute addresses describe team-alpha-tenant-api --project $PROJECT_ID --global --format="value(address)"`
+echo -e "GCLB_IP is $ALPHA_IP"
 
 # # Create Service Endpoint
 # cat <<EOF > demo-openapi.yaml
@@ -195,35 +187,20 @@ flux bootstrap github \
 # EOF
 # gcloud endpoints services deploy demo-openapi.yaml --project $PROJECT_ID
 
-# cat <<EOF > alpha-openapi.yaml
-# swagger: "2.0"
-# info:
-#   description: "Cloud Endpoints DNS"
-#   title: "Cloud Endpoints DNS"
-#   version: "1.0.0"
-# paths: {}
-# host: "team-alpha.endpoints.${PROJECT_ID}.cloud.goog"
-# x-google-endpoints:
-# - name: "team-alpha.endpoints.${PROJECT_ID}.cloud.goog"
-#   target: "${ALPHA_IP}"
-# EOF
+cat <<EOF > alpha-openapi.yaml
+swagger: "2.0"
+info:
+  description: "Cloud Endpoints DNS"
+  title: "Cloud Endpoints DNS"
+  version: "1.0.0"
+paths: {}
+host: "team-alpha.endpoints.${PROJECT_ID}.cloud.goog"
+x-google-endpoints:
+- name: "team-alpha.endpoints.${PROJECT_ID}.cloud.goog"
+  target: "${ALPHA_IP}"
+EOF
 
-# gcloud endpoints services deploy alpha-openapi.yaml --project $PROJECT_ID
-
-# cat <<EOF > bravo-openapi.yaml
-# swagger: "2.0"
-# info:
-#   description: "Cloud Endpoints DNS"
-#   title: "Cloud Endpoints DNS"
-#   version: "1.0.0"
-# paths: {}
-# host: "team-bravo.endpoints.${PROJECT_ID}.cloud.goog"
-# x-google-endpoints:
-# - name: "team-bravo.endpoints.${PROJECT_ID}.cloud.goog"
-#   target: "${BRAVO_IP}"
-# EOF
-
-# gcloud endpoints services deploy bravo-openapi.yaml --project $PROJECT_ID
+gcloud endpoints services deploy alpha-openapi.yaml --project $PROJECT_ID
 
 # # Create Certificate
 # gcloud compute ssl-certificates create whereamicert \
@@ -235,13 +212,3 @@ flux bootstrap github \
 #       --project $PROJECT_ID \
 #       --domains="team-alpha.endpoints.$PROJECT_ID.cloud.goog" \
 #       --global
-
-# gcloud compute ssl-certificates create bravo-tenant-cert \
-#       --project $PROJECT_ID \
-#       --domains="team-bravo.endpoints.$PROJECT_ID.cloud.goog" \
-#       --global
-
-# # Step 3 -> https://cloud.google.com/kubernetes-engine/docs/how-to/multi-cluster-services
-# gcloud projects add-iam-policy-binding $PROJECT_ID \
-#     --member "serviceAccount:$PROJECT_ID.svc.id.goog[gke-mcs/gke-mcs-importer]" \
-#     --role "roles/compute.networkViewer"
