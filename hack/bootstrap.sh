a2176a7afb8ad173726cc5894ebe2bd60e03571d
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2024-10-05 10:58:22 +1000

    fixing merge collateral damage

--- hack/bootstrap.sh
+++ hack/bootstrap.sh
@@ -16,17 +16,9 @@ if ! command -v flux &> /dev/null; then
     echo "flux must be installed." && exit 1
 fi
 
-<<<<<<< HEAD
-if [ -z "$GITHUB_TOKEN" ]; then
-=======
-if [ -z "$GITHUB_USER" ] || [ -z "$GITHUB_TOKEN" ]; then
->>>>>>> 9c49b08a178e9d9066e42ad27dd0957c79ccbcd9
+if [ -z "$GITHUB_TOKEN" ] || [ -z "$GCP_PROJECT_ID" ] || [ -z "$GCP_PROJECT_NUMBER" ]; then
   echo "Error required env variables are not set" && exit 1
 fi
-
-# Management GCP project configuration
-export GCP_PROJECT_ID=atomic-gke-clusters
-export GCP_PROJECT_NUMBER=1090173588460
 
 # Management GKE cluster configuration
 export CLUSTER_NAME=cluster-00
@@ -49,15 +41,13 @@ fi
 
 gcloud config set project $PROJECT_ID
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-for service in "${required_services[@]}"; do
-  if ! gcloud services list --enabled --filter="config.name=$service" --format="value(config.name)" | grep -q "$service"; then
-    gcloud services enable "$service"
-  else
-    echo "$service is already enabled"
-  fi
-done
+# for service in "${required_services[@]}"; do
+#   if ! gcloud services list --enabled --filter="config.name=$service" --format="value(config.name)" | grep -q "$service"; then
+#     gcloud services enable "$service"
+#   else
+#     echo "$service is already enabled"
+#   fi
+# done
 
 # Check and create KCC service account if it doesn't exist
 if ! gcloud iam service-accounts list --filter="email:${KCC_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" | grep -q ${KCC_SERVICE_ACCOUNT_NAME}; then
@@ -121,152 +111,11 @@ if ! gcloud iam service-accounts get-iam-policy ${KCC_SERVICE_ACCOUNT_NAME}@${PR
     --role="roles/iam.workloadIdentityUser"
 else
     echo "Workload identity binding for cnrm-controller-manager already exists"
-=======
-# Enable required services if not already enabled
-#required_services=(
-#  servicemanagement.googleapis.com
-#  servicecontrol.googleapis.com
-#  cloudresourcemanager.googleapis.com
-#  cloudkms.googleapis.com
-#  compute.googleapis.com
-#  container.googleapis.com
-#  containerregistry.googleapis.com
-#  cloudbuild.googleapis.com
-#  gkeconnect.googleapis.com
-#  gkehub.googleapis.com
-#  iam.googleapis.com
-#  mesh.googleapis.com
-#  trafficdirector.googleapis.com
-#  anthos.googleapis.com
-#  dns.googleapis.com
-#)
-
-# for service in "${required_services[@]}"; do
-#   if ! gcloud services list --enabled --filter="config.name=$service" --format="value(config.name)" | grep -q "$service"; then
-#     gcloud services enable "$service"
-#   else
-#     echo "$service is already enabled"
-#   fi
-# done
-
-# Check and create KCC service account if it doesn't exist
-if ! gcloud iam service-accounts list --filter="email:${KCC_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" | grep -q ${KCC_SERVICE_ACCOUNT_NAME}; then
-  gcloud iam service-accounts create ${KCC_SERVICE_ACCOUNT_NAME}
-  gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${KCC_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role="roles/editor"
-else
-  echo "KCC service account already exists"
-fi
-
-# Check and create SOPS service account if it doesn't exist
-if ! gcloud iam service-accounts list --filter="email:${SOPS_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" | grep -q ${SOPS_SERVICE_ACCOUNT_NAME}; then
-  gcloud iam service-accounts create ${SOPS_SERVICE_ACCOUNT_NAME}
-  gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${SOPS_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role="roles/cloudkms.cryptoKeyEncrypterDecrypter"
-
-  # TODO: Determine if this is needed - try without it.
-  # gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-  #   --member="serviceAccount:${SOPS_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-  #   --role="roles/editor"
-else
-  echo "SOPS service account already exists"
-fi
-
-
-# Check and create KMS keyring if it doesn't exist
-if ! gcloud kms keyrings list --location global --format="value(name)" | grep -q "sops"; then
-  gcloud kms keyrings create sops --location global
-fi
-
-# Check and create KMS key if it doesn't exist
-if ! gcloud kms keys list --location global --keyring sops --format="value(name)" | grep -q "sops-key"; then
-  gcloud kms keys create sops-key --location global --keyring sops --purpose encryption
-  gcloud kms keys list --location global --keyring sops
 fi
 
 NODEPOOL_NAME="np"
 ZONE="australia-southeast1-a"
-gcloud container clusters create $CLUSTER_NAME \
-    --cluster-version=$CLUSTER_VERSION \
-    --enable-dataplane-v2 \
-    --enable-ip-alias \
-    --network=$CLUSTER_VPC \
-    --subnetwork=$CLUSTER_SUBNET \
-    --node-locations=$ZONE \
-    --disk-size=50GB \
-    --total-max-nodes=4 \
-    --workload-pool=${PROJECT_ID}.svc.id.goog \
-    --workload-metadata=GKE_METADATA
-
-=======
-# Enable required services if not already enabled
-#required_services=(
-#  servicemanagement.googleapis.com
-#  servicecontrol.googleapis.com
-#  cloudresourcemanager.googleapis.com
-#  cloudkms.googleapis.com
-#  compute.googleapis.com
-#  container.googleapis.com
-#  containerregistry.googleapis.com
-#  cloudbuild.googleapis.com
-#  gkeconnect.googleapis.com
-#  gkehub.googleapis.com
-#  iam.googleapis.com
-#  mesh.googleapis.com
-#  trafficdirector.googleapis.com
-#  anthos.googleapis.com
-#  dns.googleapis.com
-#)
-
-# for service in "${required_services[@]}"; do
-#   if ! gcloud services list --enabled --filter="config.name=$service" --format="value(config.name)" | grep -q "$service"; then
-#     gcloud services enable "$service"
-#   else
-#     echo "$service is already enabled"
-#   fi
-# done
-
-# Check and create KCC service account if it doesn't exist
-if ! gcloud iam service-accounts list --filter="email:${KCC_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" | grep -q ${KCC_SERVICE_ACCOUNT_NAME}; then
-  gcloud iam service-accounts create ${KCC_SERVICE_ACCOUNT_NAME}
-  gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${KCC_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role="roles/editor"
-else
-  echo "KCC service account already exists"
-fi
-
-# Check and create SOPS service account if it doesn't exist
-if ! gcloud iam service-accounts list --filter="email:${SOPS_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" | grep -q ${SOPS_SERVICE_ACCOUNT_NAME}; then
-  gcloud iam service-accounts create ${SOPS_SERVICE_ACCOUNT_NAME}
-  gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:${SOPS_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role="roles/cloudkms.cryptoKeyEncrypterDecrypter"
-
-  # TODO: Determine if this is needed - try without it.
-  # gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-  #   --member="serviceAccount:${SOPS_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-  #   --role="roles/editor"
-else
-  echo "SOPS service account already exists"
-fi
-
-
-# Check and create KMS keyring if it doesn't exist
-if ! gcloud kms keyrings list --location global --format="value(name)" | grep -q "sops"; then
-  gcloud kms keyrings create sops --location global
-fi
-
-# Check and create KMS key if it doesn't exist
-if ! gcloud kms keys list --location global --keyring sops --format="value(name)" | grep -q "sops-key"; then
-  gcloud kms keys create sops-key --location global --keyring sops --purpose encryption
-  gcloud kms keys list --location global --keyring sops
-fi
-
-NODEPOOL_NAME="np"
-ZONE="australia-southeast1-a"
+REGION="australia-southeast1"
 gcloud container clusters create $CLUSTER_NAME \
     --cluster-version=$CLUSTER_VERSION \
     --enable-dataplane-v2 \
@@ -284,7 +133,6 @@ gcloud container node-pools delete default-pool \
     --region=australia-southeast1 \
     --quiet
 
-REGION="australia-southeast1"
 gcloud container node-pools create $NODEPOOL_NAME \
     --cluster=$CLUSTER_NAME \
     --region=$REGION \
@@ -336,7 +184,6 @@ else
   echo "Github token secret already exists"
 fi
 
-<<<<<<< HEAD
 # Create the namespace if it doesn't already exist
 kubectl get namespace flux-system >/dev/null 2>&1 || kubectl create namespace flux-system
 
@@ -355,7 +202,6 @@ gcloud compute addresses create team-alpha-tenant-api --global --project $PROJEC
 export ALPHA_IP=`gcloud compute addresses describe team-alpha-tenant-api --project $PROJECT_ID --global --format="value(address)"`
 echo -e "GCLB_IP is $ALPHA_IP"
 
-<<<<<<< HEAD
 cat <<EOF > alpha-openapi.yaml
 swagger: "2.0"
 info:
@@ -380,56 +226,3 @@ gcloud compute ssl-certificates create alpha-tenant-cert \
       --project $PROJECT_ID \
       --domains="team-alpha.endpoints.$PROJECT_ID.cloud.goog" \
       --global
-=======
-# gcloud compute addresses create team-alpha-tenant-api --global --project $PROJECT_ID
-# export ALPHA_IP=`gcloud compute addresses describe team-alpha-tenant-api --project $PROJECT_ID --global --format="value(address)"`
-# echo -e "GCLB_IP is $ALPHA_IP"
-
-# gcloud compute addresses create team-bravo-tenant-api --global --project $PROJECT_ID
-# export BRAVO_IP=`gcloud compute addresses describe team-bravo-tenant-api --project $PROJECT_ID --global --format="value(address)"`
-# echo -e "GCLB_IP is $BRAVO_IP"
-=======
->>>>>>> e9a6dc2 (remove bravo and demo-2)
-
-=======
->>>>>>> 9c49b08a178e9d9066e42ad27dd0957c79ccbcd9
-# # Create Service Endpoint
-# cat <<EOF > demo-openapi.yaml
-# swagger: "2.0"
-# info:
-#   description: "Cloud Endpoints DNS"
-#   title: "Cloud Endpoints DNS"
-#   version: "1.0.0"
-# paths: {}
-# host: "$DEMO_NAME.endpoints.${PROJECT_ID}.cloud.goog"
-# x-google-endpoints:
-# - name: "$DEMO_NAME.endpoints.${PROJECT_ID}.cloud.goog"
-#   target: "${STATIC_MCI_IP}"
-# EOF
-# gcloud endpoints services deploy demo-openapi.yaml --project $PROJECT_ID
-
-cat <<EOF > alpha-openapi.yaml
-swagger: "2.0"
-info:
-  description: "Cloud Endpoints DNS"
-  title: "Cloud Endpoints DNS"
-  version: "1.0.0"
-paths: {}
-host: "team-alpha.endpoints.${PROJECT_ID}.cloud.goog"
-x-google-endpoints:
-- name: "team-alpha.endpoints.${PROJECT_ID}.cloud.goog"
-  target: "${ALPHA_IP}"
-EOF
-
-gcloud endpoints services deploy alpha-openapi.yaml --project $PROJECT_ID
-
-# # Create Certificate
-# gcloud compute ssl-certificates create whereamicert \
-#   --project $PROJECT_ID \
-#   --domains=$DEMO_NAME.endpoints.$PROJECT_ID.cloud.goog \
-#   --global
-
-# gcloud compute ssl-certificates create alpha-tenant-cert \
-#       --project $PROJECT_ID \
-#       --domains="team-alpha.endpoints.$PROJECT_ID.cloud.goog" \
-#       --global
